f4fdc0ff5ec24031b0c21435c4db704d28af7fb2
Justin Field <fieldju@gmail.com>
2017-04-21 13:18:18 -0700

    Add new get env with default to env utils

--- src/main/java/com.fieldju.commons/EnvUtils.java
+++ src/main/java/com.fieldju.commons/EnvUtils.java
@@ -42,4 +42,17 @@ public class EnvUtils {
         return value;
     }
 
+    /**
+     * <p>Fetches a required environment variable</p>
+     *
+     * @throws IllegalStateException if the required environment variable is unset or blank
+     * @param key           the name of the environment variable to attempt to fetch
+     * @param defaultValue  the default value to use if the environment variable is unset or empty
+     * @return  {@code String} the value of the environment variable or the default, if the environment variable is unset or empty.
+     */
+    public static String getEnvWithDefault(String key, String defaultValue) {
+        String value = System.getenv(key);
+        return StringUtils.isNotBlank(value) ? value : defaultValue;
+    }
+
 }
